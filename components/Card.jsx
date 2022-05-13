860aa0fc2693cf4845caf31ee539b30690cb82c5
octrung <t.nguyenvo@eagles.oc.edu>
2022-05-13 02:12:04 -0500

    tested nav links

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,6 +1,7 @@
 export default function Card({ project }) {
+
   return (
-    <div className='grid sm:grid-cols-[1fr_auto] gap-8 max-w-7xl mb-8'>
+    <div id={project?.id} className='grid sm:grid-cols-[1fr_auto] gap-8 max-w-7xl mb-8'>
       <div className='max-w-[50ch]'>
         <h3 className='text-xl font-semibold'>
           {project.title}
@@ -11,15 +12,20 @@ export default function Card({ project }) {
         <ul className='mt-4 flex flex-wrap gap-2'>
           {
             project.technologies.map((tech) => (
-              <li className='border-[1px] border-green-600 text-zinc-500 dark:text-gray-300 px-4 py-[.5]'>
+              <li className='border-[1px] border-green-600 dark:text-green-600 text-zinc-500 px-4 py-[.5] rounded-xl'>
                 {tech}
               </li>
             ))
           }
         </ul>
-        <div className='mt-4'>
-          <a href='#' className='bg-green-600 text-zinc-50 px-4 py-1 mr-2'>Github</a>
-          <a href='#' className='bg-green-600 text-zinc-50 px-4 py-1 mr-2'>Live Site</a>
+        <div className='mt-4 flex flex-nowrap items-center gap-4'>
+          <a href='#' className=''>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="48"><path fill="none" d="M0 0h24v24H0z" /><path d="M12 2C6.475 2 2 6.475 2 12a9.994 9.994 0 0 0 6.838 9.488c.5.087.687-.213.687-.476 0-.237-.013-1.024-.013-1.862-2.512.463-3.162-.612-3.362-1.175-.113-.288-.6-1.175-1.025-1.413-.35-.187-.85-.65-.013-.662.788-.013 1.35.725 1.538 1.025.9 1.512 2.338 1.087 2.912.825.088-.65.35-1.087.638-1.337-2.225-.25-4.55-1.113-4.55-4.938 0-1.088.387-1.987 1.025-2.688-.1-.25-.45-1.275.1-2.65 0 0 .837-.262 2.75 1.026a9.28 9.28 0 0 1 2.5-.338c.85 0 1.7.112 2.5.337 1.912-1.3 2.75-1.024 2.75-1.024.55 1.375.2 2.4.1 2.65.637.7 1.025 1.587 1.025 2.687 0 3.838-2.337 4.688-4.562 4.938.362.312.675.912.675 1.85 0 1.337-.013 2.412-.013 2.75 0 .262.188.574.688.474A10.016 10.016 0 0 0 22 12c0-5.525-4.475-10-10-10z" fill="#18a34a" /></svg>
+          </a>
+          {
+            project.url !== '#' &&
+            <a href={project.url} className='bg-green-600 text-zinc-50 px-4 py-1 mr-2 rounded-2xl'>Visit Site</a>
+          }
         </div>
       </div>
       <img
--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,8 @@ const srm = {
     'NextAuth',
     'ReactQuery'
   ],
-  image: 'assets/srm-login.png'
+  image: 'assets/srm-login.png',
+  url: '#'
 }
 
 const pkmn = {
@@ -24,12 +25,14 @@ const pkmn = {
     'Next',
     'Sass',
   ],
-  image: 'assets/booster-opener.png'
+  image: 'assets/booster-opener.png',
+  url: 'https://resplendent-speculoos-b976cb.netlify.app/'
 }
 
 const cmgmt = {
+  id: 'wilshire',
   title: 'Church Management System',
-  description: 'A management system that allows users to view a directory, messages and join groups. Includes admin roles to provide extra privileges .',
+  description: 'A management system that allows users to view a directory, messages and join groups. Includes admin roles to provide extra privileges.',
   technologies: [
     'Vue',
     'Bootstrap',
@@ -38,7 +41,8 @@ const cmgmt = {
     'Node',
     'Express',
   ],
-  image: 'assets/church-login-narrow.png'
+  image: 'assets/church-login-narrow.png',
+  url: '#'
 }
 
 const projectsList = [
@@ -60,24 +64,28 @@ export default function Home() {
       </header>
       <main className='grid grid-cols-[minmax(375px,1024px)] justify-center bg-zinc-200 dark:bg-gray-900 min-h-fit'>
         <div className='px-10 py-2'>
-          <section id='bio' className='mt-2 w-full md:w-[65ch] mb-10'>
+          <section id='bio' className='mt-2 w-full md:w-[70ch] mb-10'>
             <h2 className='text-2xl font-bold mb-4'>Bio</h2>
             <p>
-              I have a BA in Computer Science from OC. My final project in school was to work with a team to develop social-media-like application for a real client. Since graduating I have been trying to stay sharp by solving problems on leetcode and learning new technologies by build projects.
+              I have a BA in Computer Science from OC. For my final project I work with a team to develop social-media-like application for a real client. Since graduating I have been trying to stay sharp by solving problems on leetcode and learning new technologies by build <a className='text-green-600 underline' href='#projects'>projects</a>.
+              {/* I have a BA in Computer Science from OC. For my final project I work with a team to develop social-media-like application for a real client <a className='text-green-600 underline' href='#wilshire'>Wilshire App</a>. Since graduating I have been trying to stay sharp by solving problems on leetcode and learning new technologies by build projects. */}
+            </p>
+            <br></br>
+            <p>
+              In my free time I like to find new music, watch critically acclaimed dramas, and I am currently learning how to cook. 🍝
 
-              In my free time I like to find new music, watch critically acclaimed dramas, and I am currently learning how to cook.
             </p>
           </section>
 
           <section id='skills' className='w-full mb-10'>
             <h2 className='text-2xl font-bold mb-4'>Skills</h2>
             <ul className='flex flex-wrap gap-2'>
-              <li className='border-[1px] border-green-600 px-4 py-[.5]'>HTML</li>
-              <li className='border-[1px] border-green-600 px-4 py-[.5]'>CSS</li>
-              <li className='border-[1px] border-green-600 px-4 py-[.5]'>React</li>
-              <li className='border-[1px] border-green-600 px-4 py-[.5]'>Next.js</li>
-              <li className='border-[1px] border-green-600 px-4 py-[.5]'>Sass</li>
-              <li className='border-[1px] border-green-600 px-4 py-[.5]'>Tailwind</li>
+              <li className='dark:text-green-600 border-[1px] border-green-600 px-4 py-[.5] rounded-xl'>HTML</li>
+              <li className='dark:text-green-600 border-[1px] border-green-600 px-4 py-[.5] rounded-xl'>CSS</li>
+              <li className='dark:text-green-600 border-[1px] border-green-600 px-4 py-[.5] rounded-xl'>React</li>
+              <li className='dark:text-green-600 border-[1px] border-green-600 px-4 py-[.5] rounded-xl'>Next.js</li>
+              <li className='dark:text-green-600 border-[1px] border-green-600 px-4 py-[.5] rounded-xl'>Sass</li>
+              <li className='dark:text-green-600 border-[1px] border-green-600 px-4 py-[.5] rounded-xl'>Tailwind</li>
             </ul>
           </section>
 
@@ -93,9 +101,9 @@ export default function Home() {
           </section>
         </div>
       </main>
-      <footer className='grid grid-cols-[minmax(375px,1024px)] justify-center'>
-        <div className='flex gap-2 items-center pl-4'>
-          {/* <h2 className='text-2xl font-bold mb-4 text-center'>find me at</h2> */}
+      <footer id='contactMe' className='grid grid-cols-[minmax(375px,1024px)] justify-center'>
+        <h2 className='text-2xl font-bold mb-4'>Contact Me</h2>
+        <div className='flex gap-2 items-center'>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="48" height="48"><path fill="none" d="M0 0h24v24H0z" /><path d="M3 3h18a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1zm17 4.238l-7.928 7.1L4 7.216V19h16V7.238zM4.511 5l7.55 6.662L19.502 5H4.511z" fill="rgba(241,196,14,1)" /></svg>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="48" height="48"><path fill="none" d="M0 0h24v24H0z" /><path d="M12 2C6.475 2 2 6.475 2 12a9.994 9.994 0 0 0 6.838 9.488c.5.087.687-.213.687-.476 0-.237-.013-1.024-.013-1.862-2.512.463-3.162-.612-3.362-1.175-.113-.288-.6-1.175-1.025-1.413-.35-.187-.85-.65-.013-.662.788-.013 1.35.725 1.538 1.025.9 1.512 2.338 1.087 2.912.825.088-.65.35-1.087.638-1.337-2.225-.25-4.55-1.113-4.55-4.938 0-1.088.387-1.987 1.025-2.688-.1-.25-.45-1.275.1-2.65 0 0 .837-.262 2.75 1.026a9.28 9.28 0 0 1 2.5-.338c.85 0 1.7.112 2.5.337 1.912-1.3 2.75-1.024 2.75-1.024.55 1.375.2 2.4.1 2.65.637.7 1.025 1.587 1.025 2.687 0 3.838-2.337 4.688-4.562 4.938.362.312.675.912.675 1.85 0 1.337-.013 2.412-.013 2.75 0 .262.188.574.688.474A10.016 10.016 0 0 0 22 12c0-5.525-4.475-10-10-10z" fill="rgba(241,196,14,1)" /></svg>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="48" height="48"><path fill="none" d="M0 0h24v24H0z" /><path d="M18.335 18.339H15.67v-4.177c0-.996-.02-2.278-1.39-2.278-1.389 0-1.601 1.084-1.601 2.205v4.25h-2.666V9.75h2.56v1.17h.035c.358-.674 1.228-1.387 2.528-1.387 2.7 0 3.2 1.778 3.2 4.091v4.715zM7.003 8.575a1.546 1.546 0 0 1-1.548-1.549 1.548 1.548 0 1 1 1.547 1.549zm1.336 9.764H5.666V9.75H8.34v8.589zM19.67 3H4.329C3.593 3 3 3.58 3 4.297v15.406C3 20.42 3.594 21 4.328 21h15.338C20.4 21 21 20.42 21 19.703V4.297C21 3.58 20.4 3 19.666 3h.003z" fill="rgba(241,196,14,1)" /></svg>
